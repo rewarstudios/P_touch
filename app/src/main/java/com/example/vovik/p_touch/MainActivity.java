a53fb60e5c83c21f31cc521ccd027edcd3899389
vovik <kaka48revar95@gmail.com>
2017-03-25 23:26:09 +0300

    добавлено определение стороны касаний(пока хуево)

--- app/src/main/java/com/example/vovik/p_touch/MainActivity.java
+++ app/src/main/java/com/example/vovik/p_touch/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.vovik.p_touch;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.TextView;
+
+import static android.R.attr.x;
 
 public class MainActivity extends Activity implements OnTouchListener {
 
@@ -13,8 +16,10 @@ public class MainActivity extends Activity implements OnTouchListener {
     TextView tv;
     int upPI = 0;
     int downPI = 0;
+    int[] a = new int[2];
     boolean inTouch = false;
     String result = "";
+    Point size = new Point();
 
     /** Called when the activity is first created. */
     @Override
@@ -41,39 +46,73 @@ public class MainActivity extends Activity implements OnTouchListener {
                 inTouch = true;
             case MotionEvent.ACTION_POINTER_DOWN: // последующие касания
                 downPI = pointerIndex;
+                for (int i = 0; i < 2; i++) {
+                    a[i] = 0;
+                    if (i < pointerCount) {
+                        if (event.getX(i) < 960) {
+                            a[i] = 1;
+                        } else {
+                            a[i] = 2;
+                        }
+                    }
+                }
                 break;
 
             case MotionEvent.ACTION_UP: // прерывание последнего касания
                 inTouch = false;
-                sb.setLength(0);
+
+                for (int i = 0; i < 2; i++) {
+                    a[i] = 0;
+                    if (i < pointerCount) {
+                        if (event.getX(i) < 960) {
+                            a[i] = 1;
+                        } else {
+                            a[i] = 2;
+                        }
+                    }
+                }
             case MotionEvent.ACTION_POINTER_UP: // прерывания касаний
                 upPI = pointerIndex;
+
+                for (int i = 0; i < 2; i++) {
+                    a[i] = 0;
+                    if (i < pointerCount) {
+                        if (event.getX(i) < 960) {
+                            a[i] = 1;
+                        } else {
+                            a[i] = 2;
+                        }
+                    }
+                }
                 break;
 
             case MotionEvent.ACTION_MOVE: // движение
-                sb.setLength(0);
-
-                for (int i = 0; i < 10; i++) {
-                    sb.append("Index = " + i);
+                for (int i = 0; i < 2; i++) {
+                    a[i] = 0;
                     if (i < pointerCount) {
-                        sb.append(", ID = " + event.getPointerId(i));
-                        sb.append(", X = " + event.getX(i));
-                        sb.append(", Y = " + event.getY(i));
-                    } else {
-                        sb.append(", ID = ");
-                        sb.append(", X = ");
-                        sb.append(", Y = ");
+                        if (event.getX(i) < 960) {
+                            a[i] = 1;
+                        } else {
+                            a[i] = 2;
+                        }
                     }
-                    sb.append("\r\n");
                 }
-                break;
-        }
-        result = "down: " + downPI + "\n" + "up: " + upPI + "\n";
 
-        if (inTouch) {
-            result += "pointerCount = " + pointerCount + "\n" + sb.toString();
+                                break;
         }
-        tv.setText(result);
+        sb.setLength(0);
+        for (int i = 0 ; i < 2 ; i++)
+        {
+            if (a[i] == 1)
+            {
+                sb.append("left");
+            }
+            if (a[i] == 2)
+            {
+                sb.append("right");
+            }
+        }
+        tv.setText(sb.toString());
         return true;
     }
 }
